3c11ee5be47cdce467a560cc53dcda240540f292
Rafael-floriano <rafael.florianoc9@gmail.com>
2023-08-26 12:43:46 -0300

    Maked configurations to request

--- src/main/java/br/com/rafael/floriano/springintegrationwithchatgpt/service/ServiceGpt.java
+++ src/main/java/br/com/rafael/floriano/springintegrationwithchatgpt/service/ServiceGpt.java
@@ -3,6 +3,7 @@ package br.com.rafael.floriano.springintegrationwithchatgpt.service;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.awt.*;
@@ -10,8 +11,27 @@ import java.util.List;
 
 @Service
 public class ServiceGpt {
+
+    private WebClient webClient;
+    //Key sempre no application.proprieties
+    String apiKey = "null";
+
+    public ServiceGpt(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("https://api.openai.com/v1/chat/completions")
+                .defaultHeader("Content-Type","application/json")
+                .defaultHeader("Authorization", String.format("Bearer %s", apiKey))
+                .build();
+    }
+
     public Mono<ChatGPTResponse> createResponde(String topic) {
-        
+        ChatGPTResquest resquest = createQuestionResquest(topic);
+
+
+    }
+
+    public ChatGPTResquest createQuestionResquest(String topic) {
+        String question = "O que devo estudar sobre: "+ topic;
+        return new ChatGPTResquest("gpt-3.5-turbo", question, 0.3, 2000, 1.0, 0.0, 0.0);
     }
 }
 
