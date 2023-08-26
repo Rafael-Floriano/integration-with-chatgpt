216be5ea51129c0a3f6ddc264cacd6df180880e0
Rafael-floriano <rafael.florianoc9@gmail.com>
2023-08-26 12:17:23 -0300

    First commit

---
+++ src/main/java/br/com/rafael/floriano/springintegrationwithchatgpt/SpringIntegrationWithChatgptApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.floriano.springintegrationwithchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationWithChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationWithChatgptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafael/floriano/springintegrationwithchatgpt/SpringIntegrationWithChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.floriano.springintegrationwithchatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationWithChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
